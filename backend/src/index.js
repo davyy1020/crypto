ad5c4c0419a1c3f9bf47decf482cc62d508e5a6b
davyy1020 <davysaputra1020@gmail.com>
2026-05-28 05:49:19 +0700

    Disable credentials in CORS to fix browser preflight blocker

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,7 @@ app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
-  credentials: true
+  credentials: false
 }));
 app.options('*', cors());
 app.use(express.json());
